38df84ddc72e1b00eac3e588502aa13972273d36
Nic <146491853+ic-nay@users.noreply.github.com>
2025-12-01 03:44:39 -0500

    Is it embarassing that I'm doing commits this late or does it simply show a drive to succeed?
    Almost done part a.

--- part2a.c
+++ part2a.c
@@ -3,13 +3,15 @@
 #include <stdlib.h>
 #include <sys/shm.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <time.h>
 
 #define RUBRICK_SIZE 5
 #define MEM_SIZE 20*sizeof(char)
 
 struct student_exam {
     int student_id;
-    int* grades;
+    int grades[25]; //I'm tired, this will do.
 };
 
 void set_rubrick(char *rubrick, char *token, int *number_of_questions){
@@ -25,27 +27,62 @@ void set_rubrick(char *rubrick, char *token, int *number_of_questions){
     }
 }
 
-void load_exam(char *exam_path, char *file_name, struct student_exam* exam){
+int load_exam(char *exam_path, char *file_name, struct student_exam* exam, int number_of_questions){
+    
     strcat(exam_path, "/");
     strcat(exam_path, file_name);
-    printf("Exam Path: %s\n", exam_path);
     FILE* exam_file_ptr = fopen(exam_path, "r");
+    
     if (exam_file_ptr == NULL) {
         printf("Unable to open exam file %s\n", exam_path);
-        exit(1);
+        return 1;
     }
-    char buffer[20]; //double expected size of student number (to be safe)
-    exam->student_id = atoi(fgets(buffer, 20, exam_file_ptr));
+
+    char buffer[10];
+
+    fgets(buffer, 10, exam_file_ptr);
+
+    exam->student_id = atoi(buffer);
+
     if (exam->student_id == 0){
-        printf("Unable to parse student number (found %s). Please ensure your files only contain a single line with a 10-digit number", buffer);
+        printf("Unable to parse student number (found %c). Please ensure your files only contain a single line with a 10-digit number", buffer);
         exit(1);
     }
+
+    for (int i = 0; i < number_of_questions; i ++){
+        exam->grades[i] = 0;
+    }
+    return 0;
+}
+
+//This ended up being a lot of params, whoops.
+void grade_exam(char *rubrick, struct student_exam* exam, int number_of_questions, int current_exam, ){
+    srand(time(NULL));
+    int i;
+    //Rubrick regrading
+    for (i = 0; i < number_of_questions; i ++){
+        sleep(((rand() % 5) + 5) / 10);
+        if ((rand() % 2) == 1){
+            rubrick[i] += 1;
+            printf("%d: Changed rubrick to have question %d be %c\n", getpid(), i, rubrick[i]);
+        }
+    }
+    for (i = 0; i < number_of_questions; i ++){
+        if (exam->grades[i] == 0){
+            sleep(((rand() % 10) + 10) / 10);
+            printf("TA %d: Grading student %d, question %d\n", getpid(), current_exam, i);
+            exam->grades[i] = getpid();
+        }
+    }
+    current_exam += 1;
+
 }
 
 int main(int argc, char *argv[]){
 
     //used often for loops;
     int i = 0;
+
 
     //established before forking and unchanging, so does not need to be shared.
     int number_of_questions = 0;
@@ -70,8 +107,6 @@ int main(int argc, char *argv[]){
         printf("Unable to open rubrick file!\n");
         return 1;
     }
-
-    printf("%s\n", argv[3]);
 
     char* GRADE_DIRECTORY = argv[3];
     
@@ -105,9 +140,9 @@ int main(int argc, char *argv[]){
     for (i = 0; i < RUBRICK_SIZE; i ++){
         printf("Rubrick item %i is %c\n", i+1, rubrick[i]);
     }
-    printf("Total of %i questions in rubrick\n", number_of_questions);
+    printf("Total of %d questions in rubrick\n", number_of_questions);
 
-    int shmid_student = shmget((key_t)1234, MEM_SIZE, 0666 | IPC_CREAT); //double the amount of memory I think we'll need, to be safe
+    int shmid_student = shmget((key_t)4321, MEM_SIZE, 0666 | IPC_CREAT); //double the amount of memory I think we'll need, to be safe
 
     if (shmid_student == -1) {
         printf("Failed to acquire shared memory!\n");
@@ -116,7 +151,7 @@ int main(int argc, char *argv[]){
 
     struct student_exam *exam = shmat(shmid_student, (void *)0, 0);
 
-    load_exam(GRADE_DIRECTORY, "exam_1.txt", exam);
+    load_exam(GRADE_DIRECTORY, "exam_1.txt", exam, number_of_questions);
 
     printf("Student number in exam_1.txt is %d\n", exam->student_id);
 
@@ -125,7 +160,7 @@ int main(int argc, char *argv[]){
     for (i = 0; i < TA_COUNT; i ++){
         pids[i] = fork();
         if (pids[i] == 0){
-
+            grade_exam(rubrick, exam, number_of_questions, 0);
             break;
         }
         printf("Process %d PID: %d\n", i, pids[i]);
@@ -146,8 +181,8 @@ int main(int argc, char *argv[]){
 
     - [x] make process
     - [ ] iterate over rubrick
-        - [ ] wait random amount of time
-            - [ ] include random library
+        - [ ] wait random amount of time between 
+            - [x] include random library
         - [ ] choose to make change
             - [ ] incremenent char by 1
         - 
